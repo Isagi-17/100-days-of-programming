a2054b7ba4b86f6058d0f3f99f1862719c8554f6
Isagi-17 <138223163+Isagi-17@users.noreply.github.com>
2023-07-24 09:58:26 +0530

    day2.py

--- day2.py
+++ day2.py
@@ -1,4 +1,6 @@
 #water requirement
-a=int(input("enter the no of hours the cooler has to cool the room"))
-print("the water required for the entered cooling hours is")
-print(2*a)
+t=int(input("enter the no of test cases"))
+for i in range (0,t):
+    a=int(input("enter the no of hours the cooler has to cool the room"))
+    print("the water required for the entered cooling hours is")
+    print(2*a)
