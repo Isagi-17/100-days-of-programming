1c41e2a76e0647c1b096a4336a1aceb0c4bc1135
Isagi-17 <138223163+Isagi-17@users.noreply.github.com>
2023-07-24 09:56:54 +0530

    day1.py

--- day1.py
+++ day1.py
@@ -1,7 +1,8 @@
 #program for total no of chapters chef has to study
-
-x=int(input("enter the courses"))
-y=int(input("enter the unnits"))
-z=int(input("enter the chapters"))
-print("total no of chapters chef has to study")
-print(x*y*z)
+t=int(input("enter the no of test cases"))
+for i in range (0,t):
+      x=int(input("enter the courses"))
+      y=int(input("enter the unnits"))
+      z=int(input("enter the chapters"))
+      print("total no of chapters chef has to study")
+      print(x*y*z)
